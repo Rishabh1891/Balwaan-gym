60fae639ccaa3d0b35c6b8b62d7aeeddf8fe14b9
Rishabh1891 <76990918+Rishabh1891@users.noreply.github.com>
2022-10-30 00:50:44 +0530

    to the gym

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
